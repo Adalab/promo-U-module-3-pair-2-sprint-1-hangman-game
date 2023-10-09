5b21e1f8b981f6dd86062cf0572acf374f1baea8
Tania <taniie_bcn@hotmail.es>
2023-10-09 11:30:40 +0200

    3.2 events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,81 @@
 //imports dependencias, imagenes, componentes, stylos
 
-import hangman from '../images/blackboard.jpg'
-import '../styles/App.scss'
-import '../fonts/src/fonts/KgTenThousandReasons-R1ll.ttf'
-
-
 function App() {
-
-//funciones, variables, handles, 
-
-//html
-  return (
-    <>
-      <img src={hangman} alt="" className="logo"/>
-      <div className="page">
-      <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
-      </header>
-      <main className="main">
-        <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
-          </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
-          </div>
-          <form className="form">
-            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
-            <input
-              autoComplete="off"
-              className="form__input"
-              maxLength="1"
-              type="text"
-              name="last-letter"
-              id="last-letter"
-            />
-          </form>
-        </section>
-        <section className="dummy error-5">
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
-        </section>
-      </main>
-    </div>
-    </>
-  )
+  //funciones, variables, handles,
+  let [numberOfErrors, setNumberOfErrors] = "";
+  numberOfErrors = 0;
+const handleClick = (event) => {
+   setNumberOfErrors (numberOfErrors + 1);
 }
 
-export default App
+  //html
+  return (
+    <>
+    <button onClick={handleClick}>Incrementar</button>
+      <div className="page">
+        <header>
+          <h1 className="header__title">Juego del ahorcado</h1>
+        </header>
+        <main className="main">
+          <section>
+            <div className="solution">
+              <h2 className="title">Solución:</h2>
+              <ul className="letters">
+                <li className="letter">k</li>
+                <li className="letter">a</li>
+                <li className="letter"></li>
+                <li className="letter">a</li>
+                <li className="letter">k</li>
+                <li className="letter">r</li>
+                <li className="letter"></li>
+                <li className="letter">k</li>
+                <li className="letter">e</li>
+                <li className="letter">r</li>
+              </ul>
+            </div>
+            <div className="error">
+              <h2 className="title">Letras falladas:</h2>
+              <ul className="letters">
+                <li className="letter">f</li>
+                <li className="letter">q</li>
+                <li className="letter">h</li>
+                <li className="letter">p</li>
+                <li className="letter">x</li>
+              </ul>
+            </div>
+            <form className="form">
+              <label className="title" htmlFor="last-letter">
+                Escribe una letra:
+              </label>
+              <input
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                type="text"
+                name="last-letter"
+                id="last-letter"
+              />
+            </form>
+          </section>
+          <section className="dummy error-5">
+            <span className="error-13 eye"></span>
+            <span className="error-12 eye"></span>
+            <span className="error-11 line"></span>
+            <span className="error-10 line"></span>
+            <span className="error-9 line"></span>
+            <span className="error-8 line"></span>
+            <span className="error-7 line"></span>
+            <span className="error-6 head"></span>
+            <span className="error-5 line"></span>
+            <span className="error-4 line"></span>
+            <span className="error-3 line"></span>
+            <span className="error-2 line"></span>
+            <span className="error-1 line"></span>
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
